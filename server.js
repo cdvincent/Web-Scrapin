a736b176cb375a46ff909bcb15434ceb179d2a38
Chris Vincent <cdvincent89@gmail.com>
2020-02-09 14:03:13 -0500

    route fixes

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const hbs = require("express-handlebars");
 
 const axios = require("axios");
 const cheerio = require("cheerio");
@@ -12,14 +11,12 @@ const PORT = 3000;
 
 const app = express();
 
-app.engine("hbs", hbs({extname: "hbs", defaultLayout: "layout", layoutsDir: __dirname + "/views/layouts/"}));
-app.set("view engine", "hbs");
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password@ds061691.mlab.com:61691/heroku_cdkwtjt7";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 app.get("/scrape", (req, res) => {
