aaca5b2fe8e32bd0257906c75cca05d4562f0bd9
Chris Vincent <cdvincent89@gmail.com>
2020-02-09 13:21:23 -0500

    heroku setup

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password@ds061691.mlab.com:61691/heroku_cdkwtjt7";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 app.get("/scrape", (req, res) => {
